d6081de11e6ff9fcbd83083b8cc5720d356b7a24
Yarely Davila <yarelydm20@gmail.com>
2024-11-13 11:16:02 -0600

    Add files via upload

---
+++ Script_4_dependientes.R
@@ -0,0 +1,31 @@
+# Importar datos de Github
+
+url <- "https://raw.githubusercontent.com/mgtagle/Met_Est_2024/main/Datos_Madera_MET.csv"
+
+madera <- read.csv(url, header = T)
+
+boxplot(madera$Peso_g ~ madera$Especie,
+        col = "lightgreen")
+
+shapiro.test(madera$Peso_g)
+
+bartlett.test(madera$Peso_g ~ madera$Especie)
+
+
+madera$peso_t <- sqrt(madera$Peso_g)
+
+boxplot(madera$peso_t ~ madera$Especie)
+
+
+t.test(madera$Peso_g ~ madera$Especie, var.equal = T)
+
+
+boxplot(madera$Lado_C ~ madera$Especie,
+        col = "pink")
+
+which(madera$Lado_C > 20.5)
+
+shapiro.test(madera$Lado_C)
+
+bartlett.test(madera$Lado_C ~ madera$Especie)
+j
